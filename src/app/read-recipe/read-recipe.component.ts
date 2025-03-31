1b4a52532a17b58d21d4982fd477c99d32a69282
Kamiko <83876411+Cod-kd@users.noreply.github.com>
2025-03-31 16:08:46 +0200

    version 1

--- src/app/read-recipe/read-recipe.component.ts
+++ src/app/read-recipe/read-recipe.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Recipe } from '../../models/Recipes';
 
 @Component({
   selector: 'app-read-recipe',
@@ -6,6 +9,23 @@ import { Component } from '@angular/core';
   templateUrl: './read-recipe.component.html',
   styleUrl: './read-recipe.component.css'
 })
-export class ReadRecipeComponent {
+export class ReadRecipeComponent implements OnInit {
+  recipeForm!: FormGroup;
+  recipe!: Recipe;
 
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.recipe = this.route.snapshot.data['recipe'];
+    
+    this.recipeForm = this.fb.group({
+      name: [{value: this.recipe.name, disabled: true}],
+      prepTimeMinutes: [{value: this.recipe.prepTimeMinutes, disabled: true}],
+      cookTimeMinutes: [{value: this.recipe.cookTimeMinutes, disabled: true}],
+      cuisine: [{value: this.recipe.cuisine, disabled: true}]
+    });
+  }
 }
--- src/app/update-recipe/update-recipe.component.ts
+++ src/app/update-recipe/update-recipe.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Recipe } from '../../models/Recipes';
 
 @Component({
   selector: 'app-update-recipe',
@@ -6,6 +9,30 @@ import { Component } from '@angular/core';
   templateUrl: './update-recipe.component.html',
   styleUrl: './update-recipe.component.css'
 })
-export class UpdateRecipeComponent {
+export class UpdateRecipeComponent implements OnInit {
+  recipeForm!: FormGroup;
+  recipe!: Recipe;
 
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.recipe = this.route.snapshot.data['recipe'];
+    
+    this.recipeForm = this.fb.group({
+      name: [this.recipe.name, Validators.required],
+      prepTimeMinutes: [this.recipe.prepTimeMinutes, [Validators.required, Validators.min(5)]],
+      cookTimeMinutes: [this.recipe.cookTimeMinutes, [Validators.required, Validators.min(5)]],
+      cuisine: [this.recipe.cuisine, Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.recipeForm.valid) {
+      console.log(this.recipeForm.value);
+      // Itt lenne a mentés implementálása
+    }
+  }
 }
